3d2a6afadabefe794686a0502b26c468ea89ef53
ankiw <ankitawankhede91@gmail.com>
2016-10-12 14:27:05 -0500

    cordova plugins

---
+++ LAB7/Source/myapp/www/js/app.js
@@ -0,0 +1,104 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+		// An alert dialog 
+    }
+
+
+
+
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+ .controller('MyCtrl', function($scope, $ionicPopup) {
+
+   $scope.showAlert = function() {
+	   
+      var alertPopup = $ionicPopup.alert({
+         title: 'Vibration Plugin',
+         template: 'Click ok to activate virbation for 3000mS'
+      });
+       	
+      alertPopup.then(function(res) {
+         navigator.vibrate(3000);
+      });
+   };
+
+})
+
+
+
+
+var app = angular.module('myreddit', ['ionic'])
+
+app.controller('RedditCtrl', function($http, $scope){
+   
+    $scope.stories = [];
+    function loadStories(params, callback){
+        $http.get('https://www.reddit.com/r/television/new/.json', {params: params})
+        .success(function(response){
+        var stories = [];
+        angular.forEach(response.data.children, function(child){
+            stories.push(child.data);
+        });
+        callback(stories);
+    });
+    }
+    
+    $scope.loadOlderStories = function(){
+        var params = {};
+        if($scope.stories.length > 0){
+            params['after'] = $scope.stories[$scope.stories.length - 1].name;
+        }
+        loadStories(params, function(olderStories){
+           $scope.stories = $scope.stories.concat(olderStories);
+           $scope.$broadcast('scroll.infiniteScrollComplete');
+        }); 
+  };
+    $scope.loadNewerStories = function(){
+        var params = {'before': $scope.stories[0].name};
+        loadStories(params, function(newrStories){
+        $scope.stories = newerStories.concat($scope.stories);
+        $scope.$broadcast('scroll.refreshComplete');
+        });
+    };
+    $scope.openLink = function(url){
+  window.open(url,'_blank');
+}
+});
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+      if(window.cordova && window.cordova.InAppBrowser){
+      window.open = window.cordova.InAppBrowser.open;
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+
+
